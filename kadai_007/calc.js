d22a7f75c8053f27bc945b5cc962e4ea1c12561a
kamocyan <165718127+kamocyan@users.noreply.github.com>
2024-05-21 22:16:33 +0900

    Add files via upload

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,13 +1,14 @@
 let num=15
 
-if (0 == num % 3) {
+if (0 == num % 3 && 0 == num % 5){
+    console.log('3と5の倍数です');
+}
+else if (0 == num % 3) {
     console.log('3の倍数です');
 }
 else if (0 == num % 5) {
     console.log('５の倍数です');
 }
-if (0 == num % 3 && 0 == num % 5){
-    console.log('3と5の倍数です');
-}else {
+else {
     console.log(num);
 }
